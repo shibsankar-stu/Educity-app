32391e5335cca53df1b16328370808a85f1aa5b1
shibsankar-stu <dasshibsankar94732@gmail.com>
2024-10-01 22:02:34 +0530

    add some pic

--- src/components/programs/programs.jsx
+++ src/components/programs/programs.jsx
@@ -1,9 +1,10 @@
 import '/src/components/programs/programs.css'
+import program1 from '/src/assets/program-1.png'
 function Program(){
     return(<>
     <div className='programs'>
         <div className="program">
-            <img src="./src/assets/program-1.png" alt="" />
+            <img src= {program1} alt="" />
             <div className="caption">
                 <img src="src/assets/program-icon-1.png" alt="" />
                 <p>Graduation Degree</p>
